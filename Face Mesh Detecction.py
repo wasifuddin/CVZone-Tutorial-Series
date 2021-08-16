b1d1140f17e6ffe8009feb7a676f832bd490ad20
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2021-08-16 11:14:13 +0600

    Update Face Mesh Detecction.py
    The code is updated according to lastest version 1.4.1

--- Face Mesh Detecction.py
+++ Face Mesh Detecction.py
@@ -1,10 +1,10 @@
 # pip install cvzone
 # pip install opencv-contrib-python
 
-import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
 import cv2
-cap = cv2.VideoCapture(1)
-detector = cvzone.FaceMeshDetector(maxFaces=1)
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=1)
 while True:
     success, img = cap.read()
     img = cv2.flip(img,1)
